7f7052584121e48272bb3b1c3ee19eb45b3c588d
Godfather-Akshay <akshaymahashabde@gmail.com>
2019-03-26 15:04:59 +0530

    replaced sqlite with mysql

--- CollegeManagement/settings.py
+++ CollegeManagement/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'CollegeManagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'collegemanagement',
+        'USER': 'root',
+        'PASSWORD': 'godfather',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
